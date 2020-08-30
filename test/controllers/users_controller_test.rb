f59acdb8feeae4bd724a30f106a8a2cd9d1dd940
Ivan Nikonorov <vanstoker@mail.ru>
2020-08-30 14:49:17 +0700

    Have some problem

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,34 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path # users_new_url
-    assert_response :success
-  end
 
+	def setup
+		@user = users(:michael)
+		@other_user = users(:archer)
+	end
+
+	test "should get new" do
+		get signup_path # users_new_url
+		assert_response :success
+	end
+
+	test "should redirect edit when not logged in" do
+		get edit_user_path(@user.id)#, params: { id: @user }
+		assert_not flash.empty?
+		assert_redirected_to login_url
+	end
+
+	#test "should redirect update when not logged in" do
+	#	patch :update, id: @user, user: { name: @user.name,
+	#																		email: @user.email }
+	#	assert_not flash.empty?
+	#	assert_redirected_to loggin_url
+	#end
+
+	test "should redirect edit when logged in as wrong user" do
+		log_in_as(@other_user)
+		get edit_user_path(@user.id)
+		assert flash.empty?
+		assert_redirected_to root_url
+	end
 end
